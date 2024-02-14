1198bffc895d270c02d0a7fb1e4934065fa930b2
Shyas <Shyass1620@gmail.com>
2024-02-14 23:04:40 +0530

    Updated

---
+++ src/Pages/Homepage/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import HomeFirstComp from './HomeFirstComp'
+import HomeSecondComp from './HomeSecondComp'
+import HomeThirdComponent from './HomeThirdComponent'
+import HomeFourthComp from './HomeFourthComp'
+
+const Home = () => {
+  return (
+  <div>
+    <div>
+           <HomeFirstComp/>
+    </div>
+    <div>
+           <HomeSecondComp/>
+    </div>
+    <div>
+           <HomeThirdComponent/>
+    </div>
+    <div>
+           <HomeFourthComp/>
+    </div>
+    </div>
+  )
+}
+
+export default Home
